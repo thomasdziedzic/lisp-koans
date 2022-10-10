6be5260a2081d062ecc501ec8b6c28b3277bafd8
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2022-10-10 07:45:40 -0500

    condition-handlers

--- koans/condition-handlers.lisp
+++ koans/condition-handlers.lisp
@@ -37,25 +37,25 @@
 (define-test type-hierarchy
   ;; Inheritance for condition types works the same way as for classes.
   (let ((condition (make-condition 'my-condition)))
-    (true-or-false? ____ (typep condition 'my-condition))
-    (true-or-false? ____ (typep condition 'condition))
-    (true-or-false? ____ (typep condition 'warning))
-    (true-or-false? ____ (typep condition 'error)))
+    (true-or-false? t (typep condition 'my-condition))
+    (true-or-false? t (typep condition 'condition))
+    (true-or-false? nil (typep condition 'warning))
+    (true-or-false? nil (typep condition 'error)))
   (let ((condition (make-condition 'my-warning)))
-    (true-or-false? ____ (typep condition 'my-warning))
-    (true-or-false? ____ (typep condition 'warning))
-    (true-or-false? ____ (typep condition 'error)))
+    (true-or-false? t (typep condition 'my-warning))
+    (true-or-false? t (typep condition 'warning))
+    (true-or-false? nil (typep condition 'error)))
   (let ((condition (make-condition 'my-serious-condition)))
-    (true-or-false? ____ (typep condition 'my-serious-condition))
-    (true-or-false? ____ (typep condition 'serious-condition))
-    (true-or-false? ____ (typep condition 'warning))
-    (true-or-false? ____ (typep condition 'error)))
+    (true-or-false? t (typep condition 'my-serious-condition))
+    (true-or-false? t (typep condition 'serious-condition))
+    (true-or-false? nil (typep condition 'warning))
+    (true-or-false? nil (typep condition 'error)))
   (let ((condition (make-condition 'my-error)))
-    (true-or-false? ____ (typep condition 'my-error))
-    (true-or-false? ____ (typep condition 'my-serious-condition))
-    (true-or-false? ____ (typep condition 'serious-condition))
-    (true-or-false? ____ (typep condition 'warning))
-    (true-or-false? ____ (typep condition 'error))))
+    (true-or-false? t (typep condition 'my-error))
+    (true-or-false? nil (typep condition 'my-serious-condition))
+    (true-or-false? t (typep condition 'serious-condition))
+    (true-or-false? nil (typep condition 'warning))
+    (true-or-false? t (typep condition 'error))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -90,7 +90,7 @@
                    (silly-condition #'handle-silly-condition)
                    (most-silly-condition #'handle-most-silly-condition))
       (signal (make-condition 'most-silly-condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:most-silly-condition :silly-condition :very-silly-condition) *list*)))
 
 (define-test handler-order
   ;; The order of binding handlers matters.
@@ -99,7 +99,7 @@
                    (very-silly-condition #'handle-very-silly-condition)
                    (most-silly-condition #'handle-most-silly-condition))
       (signal (make-condition 'most-silly-condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:most-silly-condition :very-silly-condition :silly-condition) *list*)))
 
 (define-test multiple-handler-binds
   ;; It is possible to bind handlers in steps.
@@ -108,7 +108,7 @@
                    (most-silly-condition #'handle-most-silly-condition))
       (handler-bind ((very-silly-condition #'handle-very-silly-condition))
         (signal (make-condition 'most-silly-condition))))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:most-silly-condition :silly-condition :very-silly-condition) *list*)))
 
 (define-test same-handler
   ;; The same handler may be bound multiple times.
@@ -119,7 +119,7 @@
                      (silly-condition #'handle-silly-condition)
                      (very-silly-condition #'handle-very-silly-condition))
         (signal (make-condition 'most-silly-condition))))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:silly-condition :silly-condition :very-silly-condition :silly-condition :very-silly-condition) *list*)))
 
 (define-test handler-types
   ;; A handler is not executed if it does not match the condition type.
@@ -128,7 +128,7 @@
                    (very-silly-condition #'handle-very-silly-condition)
                    (most-silly-condition #'handle-most-silly-condition))
       (signal (make-condition 'very-silly-condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:very-silly-condition :silly-condition) *list*)))
 
 (define-test handler-transfer-of-control
   ;; A handler may decline to handle the condition if it returns normally,
@@ -141,7 +141,7 @@
                                         (return-from my-block)))
                      (silly-condition #'handle-silly-condition))
         (signal (make-condition 'silly-condition))))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:silly-condition) *list*)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -161,7 +161,7 @@
     (handler-case (signal (make-condition 'my-error))
       (error (condition) (handle-error condition))
       (my-error (condition) (handle-my-error condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:error) *list*)))
 
 (define-test handler-case-order
   ;; The order of handler cases matters.
@@ -169,7 +169,7 @@
     (handler-case (signal (make-condition 'my-error))
       (my-error (condition) (handle-my-error condition))
       (error (condition) (handle-error condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:my-error) *list*)))
 
 (define-test handler-case-type
   ;; A handler cases is not executed if it does not match the condition type.
@@ -177,7 +177,7 @@
     (handler-case (signal (make-condition 'error))
       (my-error (condition) (handle-my-error condition))
       (error (condition) (handle-error condition)))
-    (assert-equal ____ *list*)))
+    (assert-equal '(:error) *list*)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -197,9 +197,9 @@
            (handler-case (divide numerator denominator)
              (division-by-zero () :division-by-zero)
              (type-error () :type-error))))
-    (assert-equal ____ (try-to-divide 6 2))
-    (assert-equal ____ (try-to-divide 6 0))
-    (assert-equal ____ (try-to-divide 6 :zero))))
+    (assert-equal 3 (try-to-divide 6 2))
+    (assert-equal :division-by-zero (try-to-divide 6 0))
+    (assert-equal :type-error (try-to-divide 6 :zero))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -212,21 +212,21 @@
     ;; See https://gitlab.com/gnu-clisp/clisp/-/issues/22
     ;; See https://github.com/armedbear/abcl/issues/177
     #-(or clisp abcl)
-    (assert-equal ____ (arithmetic-error-operands condition))
+    (assert-equal '(6 0) (arithmetic-error-operands condition))
     (let ((operation (arithmetic-error-operation condition)))
       ;; Disabled on ABCL due to a conformance bug.
       ;; See https://github.com/armedbear/abcl/issues/177
       #-abcl
-      (assert-equal ____ (funcall operation 12 4)))))
+      (assert-equal 3 (funcall operation 12 4)))))
 
 (define-test accessors-type-error
   (let ((condition (handler-case (divide 6 :zero) (type-error (c) c))))
-    (assert-equal ____ (type-error-datum condition))
+    (assert-equal :zero (type-error-datum condition))
     (let ((expected-type (type-error-expected-type condition)))
-      (true-or-false? ____ (typep :zero expected-type))
-      (true-or-false? ____ (typep 0 expected-type))
-      (true-or-false? ____ (typep "zero" expected-type))
-      (true-or-false? ____ (typep 0.0 expected-type)))))
+      (true-or-false? nil (typep :zero expected-type))
+      (true-or-false? t (typep 0 expected-type))
+      (true-or-false? nil (typep "zero" expected-type))
+      (true-or-false? t (typep 0.0 expected-type)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -254,12 +254,12 @@
   (flet ((try-log-line-type (line)
            (handler-case (log-line-type line)
              (error (condition) condition))))
-    (assert-equal ____ (try-log-line-type "TIMESTAMP 2020-05-08 16:59:39"))
-    (assert-equal ____ (try-log-line-type "HTTP GET / from 127.0.0.1"))
-    (assert-equal ____ (try-log-line-type "LOGIN administrator:hunter2"))
+    (assert-equal :timestamp (try-log-line-type "TIMESTAMP 2020-05-08 16:59:39"))
+    (assert-equal :http (try-log-line-type "HTTP GET / from 127.0.0.1"))
+    (assert-equal :login (try-log-line-type "LOGIN administrator:hunter2"))
     (let ((condition (try-log-line-type "WARNING: 95% of disk space used")))
-      (assert-equal ____ (line condition))
-      (assert-equal ____ (reason condition)))
+      (assert-equal "WARNING: 95% of disk space used" (line condition))
+      (assert-equal :unknown-log-line-type (reason condition)))
     (let ((condition (try-log-line-type 5555)))
-      (assert-equal 'string (____ condition))
-      (assert-equal 5555 (____ condition)))))
+      (assert-equal 'string (type-error-expected-type condition))
+      (assert-equal 5555 (type-error-datum condition)))))
